f52b4358d6135d6712d78debf4984e06deb8b24b
Pasquale Cioffi <c5pasqualecioffi@gmail.com>
2023-12-12 21:55:16 +0100

    added build files

---
+++ CMakeFiles/calibrazione.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibrazione.dir/calibrazione.cpp.o"
+  "calibrazione"
+  "calibrazione.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibrazione.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ distance_sensor/CMakeFiles/distance_sensor.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/src/InfraredSensor.cpp" "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/CMakeFiles/distance_sensor.dir/src/InfraredSensor.cpp.o"
+  "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/src/UltrasonicSensor.cpp" "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/CMakeFiles/distance_sensor.dir/src/UltrasonicSensor.cpp.o"
+  "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/src/impl/list_ports/list_ports_linux.cpp" "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/CMakeFiles/distance_sensor.dir/src/impl/list_ports/list_ports_linux.cpp.o"
+  "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/src/impl/list_ports/list_ports_osx.cpp" "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/CMakeFiles/distance_sensor.dir/src/impl/list_ports/list_ports_osx.cpp.o"
+  "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/src/impl/unix.cpp" "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/CMakeFiles/distance_sensor.dir/src/impl/unix.cpp.o"
+  "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/src/impl/win.cpp" "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/CMakeFiles/distance_sensor.dir/src/impl/win.cpp.o"
+  "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/src/serial.cpp" "/home/pi/Desktop/c_projects/calibrazione/distance_sensor/CMakeFiles/distance_sensor.dir/src/serial.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "distance_sensor_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "distance_sensor/include"
+  "distance_sensor/include/serial"
+  "distance_sensor"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
