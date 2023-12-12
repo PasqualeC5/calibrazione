259dd6a0ce3750b0479b0f52ef8e1e003526e2c2
OmniShadow <46645161+OmniShadow@users.noreply.github.com>
2023-12-12 20:54:58 +0100

    typo

--- main.cpp
+++ main.cpp
@@ -113,7 +113,7 @@ int main(int argc, char *argv[])
       if (use_robot)
       {
             cout << "Mettere l'ostacolo in posizione" << endl;
-            fchar c = getchar();
+            char c = getchar();
       }
 
       while (misura_attuale <= misura_massima)
