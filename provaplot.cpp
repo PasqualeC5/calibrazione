ee834489ddc4dc134fe73abd386c59bb40157992
Pasquale Cioffi <46645161+OmniShadow@users.noreply.github.com>
2023-12-19 15:29:41 +0100

    reverted

--- provaplot.cpp
+++ provaplot.cpp
@@ -4,17 +4,9 @@
 #include <time.h>
 #include <pigpio.h>
 #include <unistd.h>
-#include <iostream>
-#include <chrono>
 
 int main(int argc, char *argv[])
 {
-
-    // Get the current time point
-    auto currentTimePoint = std::chrono::high_resolution_clock::now();
-
-    // Convert the time point to microseconds since the epoch
-    auto currentTimeMicros = std::chrono::time_point_cast<std::chrono::microseconds>(currentTimePoint).time_since_epoch().count();
 
     gpioInitialise();
 
@@ -29,8 +21,8 @@ int main(int argc, char *argv[])
     /*squarewawe T = 6s, A=10mm/s*/
 
     /*time variables setup*/
-    auto startTime, currentTime, t0;
-    t0 = currentTimeMicros; // time to start analysing response
+    uint32_t startTime, currentTime, t0;
+    t0 = gpioTick(); // time to start analysing response
 
     /*process*/
     while (true)
@@ -38,13 +30,11 @@ int main(int argc, char *argv[])
         // input
         // robot.move_lin_vel_wrf(input_velocity_mms); //give 10mm/s or -10mm/s
 
-        startTime = std::chrono::time_point_cast<std::chrono::microseconds>(currentTimePoint).time_since_epoch().count();
-
+        startTime = gpioTick();
         while (1) // Run the loop for 3 seconds
         {
             // Check if 3 seconds have passed. Exit cycle if true
-            currentTime = std::chrono::time_point_cast<std::chrono::microseconds>(currentTimePoint).time_since_epoch().count();
-
+            currentTime = gpioTick();
             if (currentTime - startTime >= (period_s * 1e6) / 2)
             {
                 break; // Exit the loop after 3 seconds
