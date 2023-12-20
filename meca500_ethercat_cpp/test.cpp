30e88c358e02440327fd38c326cb019d244da96f
OmniShadow <46645161+OmniShadow@users.noreply.github.com>
2023-12-20 12:26:16 +0100

    changed test code

--- meca500_ethercat_cpp/test.cpp
+++ meca500_ethercat_cpp/test.cpp
@@ -8,12 +8,18 @@ int main(int argc, char *argv[])
     robot.set_conf(1, 1, -1);
     robot.move_pose(130, -170, 120, 90, 90, 0);
     robot.print_pose();
-    float velocity[6] = {0,2,0,0,0,0};
+    float velocity[6] = {0,0,0,0,0,0};
+    float vel = 2;
+    velocity[1]= vel;
+    int i =0;
     while (true)
     {
         robot.move_lin_vel_trf(velocity);
         usleep(3e+6);
-        robot.move_lin_vel_trf(-velocity);
+        velocity[i%3]= -vel;
+        robot.move_lin_vel_trf(velocity);
         usleep(3e+6);
+        velocity[i%3]= vel;
+        i++
     }
 }
