6f3c38a9a4e881dad757ca655c935defd2e12022
OmniShadow <46645161+OmniShadow@users.noreply.github.com>
2023-12-20 12:55:53 +0100

    .

--- controltest.cpp
+++ controltest.cpp
@@ -3,8 +3,11 @@
 #include "distance_sensor/include/InfraredSensor.hpp"
 #include "meca500_ethercat_cpp/Robot.hpp"
 
+using namespace std;
+
 int main(int argc, char *argv[])
 {
+    InfraredSensor sensor(InfraredSensor::USER_INPUT);
     Robot robot(-30, 200, 5000, "eth0", 0.0, 100);
     robot.reset_error();
     // robot.main();
@@ -13,38 +16,35 @@ int main(int argc, char *argv[])
     robot.print_pose();
     float velocity[6] = {0, 0, 0, 0, 0, 0};
     float position[6];
-    float vel = 5;
-    velocity[0] = vel;
-    int i = 0;
-    float setpoint = 50;
-    InfraredSensor sensor(InfraredSensor::USER_INPUT);
+
     float distance;
     while (true)
     {
-        robot.get_position(position);
+        robot.get_pose(position);
         distance = sensor.getDistanceInMillimeters();
+        cout << "Measured distance: " << distance << endl;
         if (distance < 255)
         {
             if (distance < 50)
             {
-                if (position[3] < robot.POS_LIMIT_INF)
-                    velocity[2] = 0;
+                if (position[0] < robot.POS_LIMIT_INF)
+                    velocity[1] = 0;
                 else
-                    velocity[2] = -10;
+                    velocity[1] = -10;
                 robot.move_lin_vel_trf(velocity);
             }
             else if (distance > 50)
             {
-                if (position[3] < robot.POS_LIMIT_SUP)
-                    velocity[2] = 0;
+                if (position[0] < robot.POS_LIMIT_SUP)
+                    velocity[1] = 0;
                 else
-                    velocity[2] = 10;
+                    velocity[1] = 10;
                 robot.move_lin_vel_trf(velocity);
             }
         }
         else
         {
-            velocity[2] = 0;
+            velocity[1] = 0;
             robot.move_lin_vel_trf(velocity);
         }
     }
