9808580ed9b5a0f28d859ca0f17619fcee6da301
OmniShadow <46645161+OmniShadow@users.noreply.github.com>
2024-01-11 14:05:19 +0100

    ritardosensore test

--- ritardosensore.cpp
+++ ritardosensore.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-#include <sys/select.h>
-#include <termios.h>
 #include <unistd.h>
-
-volatile sig_atomic_t stopFlag = 0;
-
-// Function to handle the interrupt signal
-void handleSignal(int signum) {
-    if (signum == SIGINT) {
-        stopFlag = 1;
-    }
-}
+#include <termios.h>
 
 // Function to set the terminal to non-blocking mode
 void setNonBlockingMode() {
@@ -29,31 +19,17 @@ int main() {
 
     std::cout << "Press '" << targetChar << "' to exit the loop." << std::endl;
 
-    // Register the signal handler
-    struct sigaction sa;
-    sa.sa_handler = handleSignal;
-    sigaction(SIGINT, &sa, nullptr);
+    while (true) {
+        if (read(STDIN_FILENO, &userInput, 1) > 0) {
+            std::cout << "You pressed: '" << userInput << "'. Keep going!" << std::endl;
 
-    while (!stopFlag) {
-        fd_set inputSet;
-        FD_ZERO(&inputSet);
-        FD_SET(STDIN_FILENO, &inputSet);
-
-        struct timeval timeout;
-        timeout.tv_sec = 0;
-        timeout.tv_usec = 100000;  // 100,000 microseconds (0.1 seconds)
-
-        int ready = select(STDIN_FILENO + 1, &inputSet, nullptr, nullptr, &timeout);
-
-        if (ready > 0 && FD_ISSET(STDIN_FILENO, &inputSet)) {
-            if (read(STDIN_FILENO, &userInput, 1) > 0) {
-                std::cout << "You pressed: '" << userInput << "'. Keep going!" << std::endl;
-
-                if (userInput == targetChar) {
-                    stopFlag = 1;
-                }
+            if (userInput == targetChar) {
+                break;  // Exit the loop when the target character is pressed
             }
         }
+
+        // Add some delay to avoid high CPU usage
+        usleep(100000);  // Sleep for 100,000 microseconds (0.1 seconds)
     }
 
     std::cout << "Exiting the loop. You pressed '" << targetChar << "'." << std::endl;
