fee1d19134e5dcedd039f20e23171391fbe2e8e8
francesco1203 <francesco.schettini12@gmail.com>
2024-01-31 00:13:27 +0100

    Francesco's push 31-01

--- plot_dev_std.py
+++ plot_dev_std.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Write path to your file
-csv_file_path = 'measurements/infrared/cartoncinonero_calibrazione_estratto/stats/stats.csv'
+surface = "cartoncinonerocalibratoCopia"
+csv_file_path = 'measurements/infrared/-CARTONCINI/'+ surface + '/stats/stats.csv'
 
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv(csv_file_path)
@@ -19,5 +20,5 @@ plt.xlabel('Valore atteso[mm]')
 plt.ylabel('Dev_std')
 
 # Show the plot
-plt.savefig('dev_std.png')
+plt.savefig('dev_std_' + surface + '.png')
 plt.show()
